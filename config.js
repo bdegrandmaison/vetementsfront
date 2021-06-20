2d1df8baeaba18b0c902e658c5975edc7cf9aa3a
B. de Grandmaison <bdegrandmaison@hotmail.fr>
2021-06-20 22:16:54 +0200

    Heroku bak port

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const endpoint = `https://vetementsbackend.herokuapp.com/api/graphql`;
-export const prodEndpoint = `https://vetementsbackend.herokuapp.com/api/graphql`;
+export const endpoint = `https://vetementsbackend.herokuapp.com:54474/api/graphql`;
+export const prodEndpoint = `https://vetementsbackend.herokuapp.com:54474/api/graphql`;
 export const perPage = 4;
