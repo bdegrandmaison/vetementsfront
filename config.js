dc9ae499b3d3c5fc3dfa59b112b651fbd2d2f7c3
B. de Grandmaison <bdegrandmaison@hotmail.fr>
2021-06-21 06:09:47 +0200

    fixed url

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const endpoint = `http://vetements-env.eba-e3bmg3um.eu-west-3.elasticbeanstalk.com:8080/api/graphql`;
-export const prodEndpoint = `http://vetements-env.eba-e3bmg3um.eu-west-3.elasticbeanstalk.com:8080/api/graphql`;
+export const endpoint = `http://vetements-env.eba-e3bmg3um.eu-west-3.elasticbeanstalk.com/api/graphql`;
+export const prodEndpoint = `http://vetements-env.eba-e3bmg3um.eu-west-3.elasticbeanstalk.com/api/graphql`;
 export const perPage = 4;
